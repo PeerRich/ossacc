24e01b3a22b41fecf5de50a018c19116d50ff98d
Peer Richelsen <peer@cal.com>
2023-11-29 11:48:26 +0000

    og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,10 +37,17 @@ export default function RootLayout({
       )}
     >
       <head>
+        <meta property="og:image" content="https://ossacc.com/og-image.jpg" />
+
         <link
           rel="preconnect"
           href="https://cdn.fontshare.com"
           crossOrigin="anonymous"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀🤒🎅</text></svg>"
         />
         <link
           rel="stylesheet"
