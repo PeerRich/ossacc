ad0af5758d95b77f573c5d3cbf8f0d92229846d3
Peer Richelsen <peer@cal.com>
2024-11-14 15:29:41 +0100

    nit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,12 @@
 import { Author } from '@/components/Author'
 import { Footer } from '@/components/Footer'
-import { FreeChapters } from '@/components/FreeChapters'
-import Friends from '@/components/Friends'
-import { Hero } from '@/components/Hero'
 import { Introduction } from '@/components/Introduction'
-import { NavBar } from '@/components/NavBar'
-import { Pricing } from '@/components/Pricing'
-import { Resources } from '@/components/Resources'
-import { Screencasts } from '@/components/Screencasts'
-import { TableOfContents } from '@/components/TableOfContents'
-import { Testimonial } from '@/components/Testimonial'
-import { Testimonials } from '@/components/Testimonials'
-import avatarImage1 from '@/images/avatars/avatar-1.png'
-import avatarImage2 from '@/images/avatars/avatar-2.png'
 
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
       <Introduction />
-      <NavBar />
-      <TableOfContents />
-      {/* */}
-      {/*  */}
-      {/* <Testimonial
-        id="testimonial-from-tommy-stroman"
-        author={{
-          name: 'Tommy Stroman',
-          role: 'Front-end developer',
-          image: avatarImage1,
-        }}
-      >
-        <p>
-          “I didn’t know a thing about icon design until I read this book. Now I
-          can create any icon I need in no time. Great resource!”
-        </p>
-      </Testimonial> */}
-      {/* <Screencasts />
-      <Testimonial
-        id="testimonial-from-gerardo-stark"
-        author={{
-          name: 'Gerardo Stark',
-          role: 'Creator of Pandemicons',
-          image: avatarImage2,
-        }}
-      >
-        <p>
-          “I’ve tried to create my own icons in the past but quickly got
-          frustrated and gave up. Now I sell my own custom icon sets online.”
-        </p>
-      </Testimonial>
-      <FreeChapters />
-      <Pricing />
-      <Friends /> */}
-      <Testimonials />
-
-      <Resources />
-      {/* 
-      <Author /> */}
+      <Author />
       <Footer />
     </>
   )
